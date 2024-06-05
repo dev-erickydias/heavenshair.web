33e3f95680acd328b45a012399c053334726bb6e
Ericky Dias <erickyhenriquesd@gmail.com>
2024-06-05 12:15:58 +0100

    add component de navegação no layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,9 +12,9 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="pt-nr">
+    <html lang="pt-br">
       <body className="page">
-        
+        <NavMenu />
         {children}
         </body>
     </html>

967f8af1c8c4bf560c0011b3905b6c694cd27a4a
Ericky Dias <erickyhenriquesd@gmail.com>
2024-06-10 12:36:50 +0100

    refiz o contatos

--- src/components/contatoForm/ContatoForm.jsx
+++ src/components/contatoForm/ContatoForm.jsx
@@ -5,36 +5,13 @@ import Link from 'next/link';
 import CustomImage from '../customImage/CustomImage';
 export default function ContactForm(){
   return (
-    <div className="contact-form">
-      <h2 className="contact-form__title">Entre em contato conosco!</h2>
-      <p className="contact-form__description">
-        Preencha o formulário ou nos envie diretamente pelas redes sociais, entraremos em contato assim que possível.
+    <div className="contact">
+      <h2 className="contact__title">Entre em contato conosco!</h2>
+      <p className="contact__description">
+      Entre em contato conosco diretamente pelas redes sociais
       </p>
-      <form className='contact-form__form'>
-        <input
-          type="text"
-          name="firstName"
-          placeholder="Nome"
-          className="contact-form__input"
-          required
-        />
-        <input
-          type="text"
-          name="lastName"
-          placeholder="Sobrenome"
-          className="contact-form__input"
-          required
-        />
-        <textarea
-          name="message"
-          placeholder="Escreva sua mensagem aqui :)"
-          className="contact-form__textarea"
-          rows="5"
-          required
-        ></textarea>
-        <button type="submit" className="contact-form__button">Enviar</button>
-      </form>
-      <div className="contact-form__social">
+      
+      <div className="contact__social">
         <Link target='_blank' href="https://www.instagram.com/cbheavenshair/"><CustomImage src="/icon/instagram.png"/></Link>
         <Link target='_blank' href="https://www.instagram.com/cbheavenshair/"><CustomImage src="/icon/facebook.png"/></Link>
         <Link target='_blank' href="mailto:cbeheavenshair@gmail.com"><CustomImage src="/icon/email.png"/></Link>

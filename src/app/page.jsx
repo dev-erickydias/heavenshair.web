1086742537cbb0999357970f9deb1317a070937a
Ericky Dias <erickyhenriquesd@gmail.com>
2024-06-10 12:29:55 +0100

    add component contatos

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,6 +3,7 @@ import SaibaMaisCursos from "@/components/saibaMaisCursos/SaibaMaisCursos";
 import Header from "../components/header/header/Header";
 import Servicos from "../components/servicos/Servicos";
 import Gallery from "@/components/gallery/Gallery";
+import ContactForm from "@/components/contatoForm/ContatoForm";
 export default function Home() {
   return (
     <main>
@@ -10,6 +11,7 @@ export default function Home() {
       <Servicos />
       <SaibaMaisCursos/>
       <Gallery/>
+      <ContactForm/>
     </main>
   );
 }
